998683ba01c533cbf457787f229674a8daa5fe46
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2022-07-03 20:26:30 +0700

    first commit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    string username;
+    cout<<"Hello there!"<<endl;
+    cin>>username;
+    cout<<"Hello, "<<username;
+}
